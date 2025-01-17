0ac81fbcde724716fe39dffde227fc7d7d72000d
Sai Seyyadri <sai.seyyadri@gmail.com>
2025-01-17 14:27:12 -0500

    Adding mongodb.js

---
+++ pages/api/test.js
@@ -0,0 +1,12 @@
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  try {
+    const client = await clientPromise;
+    const db = client.db('RoommateConnectCluster'); 
+    const collections = await db.listCollections().toArray();
+    res.status(200).json({ collections });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
